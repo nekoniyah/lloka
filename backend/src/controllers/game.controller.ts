bb669eb188553dcec72dbe184296d22004ee3386
Nekoniyah <beastish.maki@gmail.com>
2026-05-23 13:35:15 +0200

    added "general" game routes and editor game routes

--- backend/src/controllers/game.controller.ts
+++ backend/src/controllers/game.controller.ts
@@ -147,6 +147,33 @@ export async function putEditorGameHandler(
   await reply.send({ message: "ok" });
 }
 
+export async function deleteEditorGameHandler(
+  request: FastifyRequest<{
+    Params: { id: string };
+  }>,
+  reply: FastifyReply,
+) {
+  const user = await request.session?.require()!;
+
+  const game = await request.server.database.game.findUnique({
+    where: {
+      ownerId: user.id,
+      id: request.params.id,
+    },
+  });
+
+  if (!game) return reply.status(404).send({ error: "Game not found" });
+
+  await request.server.database.game.delete({
+    where: {
+      ownerId: user.id,
+      id: request.params.id,
+    },
+  });
+
+  await reply.send({ message: "ok" });
+}
+
 export async function getEditorGameElementsHandler(
   request: FastifyRequest<{
     Params: { id: string };
@@ -167,8 +194,132 @@ export async function getEditorGameElementsHandler(
   await reply.send(
     game.gameElements.map((element) => ({
       ...element,
-      createdAt: element.createdAt.toISOString(),
-      updatedAt: element.updatedAt.toISOString(),
+      createdAt: element.createdAt.getTime(),
+      updatedAt: element.updatedAt.getTime(),
     })),
   );
+}
+
+export async function postEditorGameElementHandler(
+  request: FastifyRequest<{
+    Params: { id: string };
+    Body: {
+      name: string;
+      frontImage: string;
+      type: string;
+    };
+  }>,
+  reply: FastifyReply,
+) {
+  const user = await request.session?.require()!;
+
+  const game = await request.server.database.game.findUnique({
+    where: {
+      ownerId: user.id,
+      id: request.params.id,
+    },
+  });
+
+  if (!game) return reply.status(404).send({ error: "Game not found" });
+
+  const gameElement = await request.server.database.gameElement.create({
+    data: {
+      name: request.body.name,
+      imageUrl: request.body.frontImage,
+      gameId: request.params.id,
+      type: request.body.type,
+    },
+  });
+
+  await reply.send({
+    message: "ok",
+    id: gameElement.id,
+  });
+}
+
+export async function putEditorGameElementHandler(
+  request: FastifyRequest<{
+    Params: { id: string; elementId: string };
+    Body: {
+      name?: string;
+      frontImage?: string;
+      backImage?: string;
+      scale?: number;
+      x?: number;
+      y?: number;
+      zIndex?: number;
+      width?: number;
+      height?: number;
+      type?: string;
+      rotation?: number;
+      opacity?: number;
+      visible?: boolean;
+      locked?: boolean;
+    };
+  }>,
+  reply: FastifyReply,
+) {
+  const user = await request.session?.require()!;
+
+  const game = await request.server.database.game.findUnique({
+    where: {
+      ownerId: user.id,
+      id: request.params.id,
+    },
+  });
+
+  if (!game) return reply.status(404).send({ error: "Game not found" });
+
+  await request.server.database.gameElement.update({
+    where: {
+      id: request.params.elementId,
+      gameId: request.params.id,
+    },
+    data: {
+      name: request.body.name,
+      imageUrl: request.body.frontImage,
+      type: request.body.type,
+      scale: request.body.scale,
+      x: request.body.x,
+      y: request.body.y,
+      zIndex: request.body.zIndex,
+      width: request.body.width,
+      height: request.body.height,
+      rotation: request.body.rotation,
+      opacity: request.body.opacity,
+      visible: request.body.visible,
+      locked: request.body.locked,
+      backUrl: request.body.backImage,
+      updatedAt: new Date(Date.now()),
+    },
+  });
+
+  await reply.send({ message: "ok" });
+}
+
+export async function deleteEditorGameElementHandler(
+  request: FastifyRequest<{
+    Params: { id: string; elementId: string };
+  }>,
+  reply: FastifyReply,
+) {
+  const user = await request.session?.require()!;
+
+  const game = await request.server.database.game.findUnique({
+    where: {
+      ownerId: user.id,
+      id: request.params.id,
+    },
+  });
+
+  if (!game) return reply.status(404).send({ error: "Game not found" });
+
+  await request.server.database.gameElement.delete({
+    where: {
+      id: request.params.elementId,
+      gameId: request.params.id,
+    },
+  });
+
+  await reply.send({ message: "ok" });
 }
